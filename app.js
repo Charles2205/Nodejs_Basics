22058a01c3680f7a60aae122e76eb615400caee5
Charles2205 <cewudzie0@gmail.com>
2023-08-08 13:12:27 +0000

    Node

--- app.js
+++ app.js
@@ -6,24 +6,39 @@ const http = require('http');
 // importing file module 
 const fileSystem = require('fs');
 
+// importing URL module
+const url = require('url');
+
 // setting up a port number for the server
 const PORT = 1122
 
 // creating the server
 
 const server=http.createServer((req,res)=>{
-    console.log(req);
     // setHeader to set the content-type header
     res.setHeader('Content-Type', 'text/html')
-    
-    // reading the file or data from the index html file
-    const data=fileSystem.readFileSync(__dirname+ '/index.html',(encoding="utf8"))
 
+    const resUrl = url.parse(req.url).pathname;
+
+    // reading the file or data from the index html file
+    let data=''
+
+    // a condition to check the pages/route in the webServer
+    if (resUrl === '/'){
+         data=fileSystem.readFileSync(__dirname+'/index.html',(encoding="utf8"))
+    }else if(resUrl ==="/contact"){
+         data=fileSystem.readFileSync(__dirname+'/contact.html',(encoding="utf8"))
+    }else if(resUrl ==="/about"){
+        data=fileSystem.readFileSync(__dirname+'/about.html',(encoding="utf8")) 
+    }
+    console.log(resUrl);
+    
     // sending the data to the server
     res.write(data)
 
     // end server
     res.end();
+    
 })
 
 // calling the server
