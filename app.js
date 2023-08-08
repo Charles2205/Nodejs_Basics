edbc79f02d9b30d674cacd8fcdc2c7102c7e15d2
Charles2205 <cewudzie0@gmail.com>
2023-08-08 12:41:31 +0000

    Node

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 // Creating a server with NodeJS
+
+// requesting or importing http module
 const http = require('http');
+
+// importing file module 
+const fileSystem = require('fs');
 
 // setting up a port number for the server
 const PORT = 1122
 
 // creating the server
+
 const server=http.createServer((req,res)=>{
-res.write('<h1 style ="color:blue">Hello Server<h1>')
-res.end();
+    console.log(req);
+    // setHeader to set the content-type header
+    res.setHeader('Content-Type', 'text/html')
+    
+    // reading the file or data from the index html file
+    const data=fileSystem.readFileSync(__dirname+ '/index.html',(encoding="utf8"))
+
+    // sending the data to the server
+    res.write(data)
+
+    // end server
+    res.end();
 })
 
 // calling the server
